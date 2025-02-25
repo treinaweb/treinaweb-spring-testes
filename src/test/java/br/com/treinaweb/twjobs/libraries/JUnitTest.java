ca033f68f350389dd479e8f52b8920c086e63cbe
Cleyson Lima <cleysonph@gmail.com>
2025-02-25 17:47:32 -0300

    02:06 - Conhecendo o JUnit

---
+++ src/test/java/br/com/treinaweb/twjobs/libraries/JUnitTest.java
@@ -0,0 +1,18 @@
+package br.com.treinaweb.twjobs.libraries;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class JUnitTest {
+
+    @Test
+    void testSomething() {
+        var actual = "TEST";
+        var expected = "TEST";
+        
+        assertEquals(expected, actual, "Algo deu errado");
+        assertNull(null);
+        assertTrue(true);
+    }
+    
+}
