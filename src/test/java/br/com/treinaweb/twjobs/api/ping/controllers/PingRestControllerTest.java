dc959d226dc9624792ce1220df8d9ae210da545a
Cleyson Lima <cleysonph@gmail.com>
2025-02-26 20:02:47 -0300

    07:19 - Testando o PingRestController

---
+++ src/test/java/br/com/treinaweb/twjobs/api/ping/controllers/PingRestControllerTest.java
@@ -0,0 +1,38 @@
+package br.com.treinaweb.twjobs.api.ping.controllers;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Tags;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.web.servlet.MockMvc;
+
+@AutoConfigureMockMvc
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class PingRestControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @Tags({@Tag("controller"), @Tag("slow")})
+    @DisplayName("when GET to /api/ping then return 200")
+    void whenGetToApiPingThenReturns200() throws Exception {
+        var requestBuilder = get("/api/ping");
+        mockMvc.perform(requestBuilder)
+            .andExpect(status().isOk())
+            .andExpect(content().contentType("application/json"))
+            .andExpect(jsonPath("$.message").value("pong"))
+            .andDo(print());
+    }
+
+}
